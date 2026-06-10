c173f4a36773cc931251ac6e24c5f6bc5c202d16
Luciana Gonzalez <lucianagonzalez931@gmail.com>
2026-06-10 15:58:17 -0300

    Mis cambios antes del pull

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from .producto import Producto
-from .categoria import Categoria
 from .atributos import Atributo
 from .ranking import Ranking
 from .relaciones import Relacion
+from .categoria import Categoria
+
